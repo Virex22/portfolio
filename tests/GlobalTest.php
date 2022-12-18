d257b3e4b01cb2150164397d6a3ae025250119b9
Virex22 <vincentremy222@gmail.com>
2022-12-18 11:00:37 +0100

    📦 feat : Setup the PHPUnit tools
    * Add PHPUnit on project
    * Add PHPUnit installation on CI
    * Check PHPUnit tests on CI

---
+++ tests/GlobalTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class GlobalTest extends TestCase
+{
+    public function testPHPUnit(): void
+    {
+        $this->assertTrue(true);
+    }
+}
