9bd814fb20991ab00d0d328d6993620a180a43ce
Virex22 <vincentremy222@gmail.com>
2023-02-14 23:00:33 +0100

    🐛 fix : add databases on CI if not exist

---
+++ tests/ConfigurationTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Tests;
+
+use App\Helper\ConfigurationHelper;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Translation\Exception\NotFoundResourceException;
+
+// test the configuration entity
+class ConfigurationTest extends KernelTestCase
+{
+
+    public function testConfiguration()
+    {
+        $this->bootKernel();
+
+        // get the configuration helper
+        $configurationHelper = $this->getContainer()->get(ConfigurationHelper::class);
+
+        // test the configuration helper
+        $this->assertFalse($configurationHelper->existConfiguration('test'));
+        $configurationHelper->setConfiguration('test', 'test');
+        $this->assertTrue($configurationHelper->existConfiguration('test'));
+        $this->assertEquals('test', $configurationHelper->getConfiguration('test'));
+        $configurationHelper->removeConfiguration('test');
+        $this->assertFalse($configurationHelper->existConfiguration('test'));
+    }
+
+    public function testConfigurationException()
+    {
+        $this->bootKernel();
+
+        // get the configuration helper
+        $configurationHelper = $this->getContainer()->get(ConfigurationHelper::class);
+
+        // test the configuration helper
+        $this->expectException(NotFoundResourceException::class);
+        $configurationHelper->getConfiguration('test');
+    }
+}
